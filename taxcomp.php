7e561311fc25ef2573cdd667581a58162689fac4
kgpelayo <kgpelayo1@up.edu.ph>
2022-05-17 15:41:06 +0800

    Test 1
    First iteration of html php code

---
+++ taxcomp.php
@@ -0,0 +1,86 @@
+<?php
+
+function grossIncome($monthlyIncome){
+    $totalIncome = $monthlyIncome*13;
+    return $totalIncome;
+}
+
+function totalDeductions($monthlyIncome, $govtEmployed, $numberOfDependents){
+
+    if ($govtEmployed == "Y"){
+        $insuranceDeduction = $monthlyIncome*0.09;
+    }
+    else {
+        $insuranceDeduction = $monthlyIncome*0.11;
+    }
+
+    if ($numberOfDependents <=4){
+        $dependentDeduction = $numberOfDependents*50000;
+    }
+    
+    else {
+        $dependentDeduction = 200000;
+    }
+
+    $pagIbigDeductions = $monthlyIncome*0.01375;
+    $philHealthDeductions = $monthlyIncome*0.035;
+    $personalExemption = 250000;
+    $deductions = $dependentDeduction + $personalExemption + ($insuranceDeduction + $philHealthDeductions + $pagIbigDeductions)*12;
+    return $deductions;
+}
+
+function netTaxableIncome($allIncome, $allDeductions){
+    $TaxableIncome = $allIncome - $allDeductions;
+    return $TaxableIncome;
+}
+
+function payableTax($taxableIncome){
+
+    $Income = $taxableIncome;
+
+    $tax="";
+
+        if ($Income <= 250000){
+            $tax = 0;
+        }
+        else if (250000 < $Income && $Income <= 400000){
+            $tax = ($Income-250000)*0.2;
+        }
+        else if (400000 < $Income && $Income <= 800000){
+            $tax = 30000 + ($Income-400000)*0.25;
+        }
+        else if (800000 < $Income && $Income <= 2000000){
+            $tax = 130000 + ($Income-800000)*0.3;
+        }
+        else if (2000000 < $Income && $Income <= 8000000){
+            $tax = 490000 + ($Income-2000000)*0.32;
+        }
+        else if (8000000 < $Income){
+            $tax = 2410000 + ($Income-8000000)*0.35;
+        }
+        
+        return $tax;
+}
+
+function getValue(){
+
+    $monthlyIncome = $_GET['monthlyIncome'];
+    $govtEmployed = $_GET['govtEmployed'];
+    $numberOfDependents = $_GET['numberOfDependents'];
+
+    $totalIncome = grossIncome($monthlyIncome);
+
+    $deductions = totalDeductions($monthlyIncome, $govtEmployed, $numberOfDependents);
+
+    $taxableIncome = netTaxableIncome($totalIncome, $deductions);
+
+    $tax = payableTax($taxableIncome);
+    
+    echo "\nTotal Income = ".$totalIncome;
+    echo "\nTotal Deductions = ".$deductions;
+    echo "\nTaxable Income = ".$taxableIncome;
+    echo "\nPayable Tax = ".$tax;
+
+}
+
+?>
